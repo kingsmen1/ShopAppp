73c36b9f772cbdfd59fb88c8b3b03f447db20723
kingsmen1 <90970163+kingsmen1@users.noreply.github.com>
2022-02-11 19:28:44 +0530

    196

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/products_provider.dart';
-
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopapp/providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -9,11 +11,13 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   final productId = ModalRoute.of(context).settings.arguments as String; // is the id!
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(context, listen: false)
+        .findById(productId); // is the id!
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(productId),
+        title: Text(loadedProduct.title),
       ),
     );
   }
