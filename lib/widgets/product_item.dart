654f6e72f1e8e4ee4d0c5f1e8bc3e95394fd0396
kingsmen1 <90970163+kingsmen1@users.noreply.github.com>
2022-02-12 20:37:33 +0530

    199

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -17,40 +17,43 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context );
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
-                arguments: product.id);
-          },
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
-          ),
-        ),
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon:  Icon(product.isFavorite?Icons.favorite:Icons.favorite_border),
-            color: Theme.of(context).accentColor,
-            onPressed: product.toggleFavoriteStatus,
-          ),
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-          trailing: IconButton(
-            icon:const Icon(
-              Icons.shopping_cart,
+    final product = Provider.of<Product>(context , listen: false );
+    return  ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                  arguments: product.id);
+            },
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
             ),
-            onPressed: () {},
-            color: Theme.of(context).accentColor,
+          ),
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: Consumer<Product>(//this is alternative to Provider.of(mainly used by
+              // individula child widget in the widget tree to avoid re-reun of build method)
+              builder: (ctx, producct,child)=>IconButton(
+                icon:  Icon(producct.isFavorite?Icons.favorite:Icons.favorite_border),
+                color: Theme.of(context).accentColor,
+                onPressed: product.toggleFavoriteStatus,
+              ),
+            ),
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+              icon:const Icon(
+                Icons.shopping_cart,
+              ),
+              onPressed: () {},
+              color: Theme.of(context).accentColor,
+            ),
           ),
         ),
-      ),
     );
   }
 }
