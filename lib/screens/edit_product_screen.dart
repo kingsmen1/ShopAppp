3122952e7a749b10ed8cc58595be8682c8656f19
kingsmen1 <90970163+kingsmen1@users.noreply.github.com>
2022-02-18 20:20:38 +0530

    223

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -2,22 +2,41 @@ import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const routeName = "/edit_product";
+
   @override
   _EditProductScreenState createState() => _EditProductScreenState();
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final _pricefocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:const Text('Edit Product')),
+      appBar: AppBar(title: const Text('Edit Product')),
       body: Form(
-        child: TextFormField(
-          decoration: const InputDecoration(labelText: 'Title',),
-          textInputAction: TextInputAction.next,
-
-
-        ),
+        child: SingleChildScrollView(
+            child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Title',
+              ),
+              textInputAction: TextInputAction.next,
+              onFieldSubmitted: (_){
+                FocusScope.of(context).requestFocus(_pricefocusNode);
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Price ',
+              ),
+              textInputAction: TextInputAction.next,
+              keyboardType: TextInputType.number,
+              focusNode: _pricefocusNode,
+            )
+          ],
+        )),
       ),
     );
   }
