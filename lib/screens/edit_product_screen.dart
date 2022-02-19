65deb7e8e0aff8248597d80261e445807b774527
kingsmen1 <90970163+kingsmen1@users.noreply.github.com>
2022-02-19 12:02:19 +0530

    228

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -33,6 +33,10 @@ class _EditProductScreenState extends State<EditProductScreen> {
   }*/
 
   void _saveForm() {
+    final isValid = _form.currentState.validate();
+    if(!isValid){
+      return;
+    }
     _form.currentState.save();
     print(_editedProduct.title);
     print(_editedProduct.price);
@@ -71,6 +75,11 @@ class _EditProductScreenState extends State<EditProductScreen> {
               textInputAction: TextInputAction.next,
               onFieldSubmitted: (_) {
                 FocusScope.of(context).requestFocus(_pricefocusNode);
+              },
+              validator: (value){
+                if(value.isEmpty){
+                  return 'Please Provide a value';
+                }return null ;
               },
               onSaved: (value) {
                 _editedProduct = Product(
